7b31dbbafccec93876e60681c890d6602a66adf9
nolka <xternalx@gmail.com>
2020-09-30 12:02:49 +0700

    Added UserInput reader

---
+++ src/Reader/UserInputReader.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TranslationLoader\Reader;
+
+use TranslationLoader\Data\DataRow;
+
+/**
+ * Used for creating translations from cli, or app controller, or service, etc...
+ * @package TranslationLoader\Reader
+ */
+class UserInputReader implements TranslationReaderInterface
+{
+    /** @var DataRow Datarow for translation */
+    protected DataRow $dataRow;
+
+    /**
+     * HardcodedReader constructor.
+     * @param string $sourceLangCode
+     * @param string $sourceValue
+     * @param string $destLangCode
+     * @param string $destValue
+     */
+    public function __construct(string $sourceLangCode, string $sourceValue, string $destLangCode, string $destValue)
+    {
+        $this->dataRow = new DataRow();
+        $this->dataRow->sourceLangCode = $sourceLangCode;
+        $this->dataRow->sourceValue = $sourceValue;
+        $this->dataRow->destLangCode = $destLangCode;
+        $this->dataRow->destValue = $destValue;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function read(): \Generator
+    {
+        yield $this->dataRow;
+    }
+}
